88c0b2d91c3413ad92a916f3093a2938ae7af099
Hrvoje <hrvooje@gmail.com>
2019-01-18 15:01:14 +0100

    Added email sending

--- kadaStizeDjecji3.py
+++ kadaStizeDjecji3.py
@@ -5,6 +5,8 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import re
+import smtplib
+from envelopes import Envelope
 
 headers = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 \
@@ -36,6 +38,27 @@ if myDate:
 else:
     print('No match')
 
+emaillMessage = 'Djecji doplatak sjeda ' + myDate.group()
 
+# Send email from gmail to gmail without authentication
+envelope = Envelope(
+    from_addr=(u'djecji@sjeda.com', u'djecji sjeda'),
+    to_addr=(u'hrvooje@gmail.com', u'hrvooje '),
+    subject=u'Kada sjeda djecji',
+    text_body=emaillMessage
+)
+#envelope.add_attachment('your_attachment.bin')
+# Send the envelope using an ad-hoc connection...
+envelope.send('aspmx.l.google.com', login='',
+              password='', tls=False)
+
+# # Gmail username and password required
+# server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+# server.login("your username", "your password")
+# server.sendmail(
+#   "from@address.com", 
+#   "to@address.com", 
+#   "this message is from python")
+# server.quit()
 
 
